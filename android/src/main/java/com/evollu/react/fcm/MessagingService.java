e706109a0e874fd721d84d6c8f2124f3635dd7ca
Ali Zahid <ali.zahid@live.com>
2016-11-25 14:24:15 +0500

    BigPicture support added

--- android/src/main/java/com/evollu/react/fcm/MessagingService.java
+++ android/src/main/java/com/evollu/react/fcm/MessagingService.java
@@ -1,11 +1,24 @@
 package com.evollu.react.fcm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MessagingService extends FirebaseMessagingService {
 
@@ -14,8 +27,67 @@ public class MessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d(TAG, "Remote message received");
-        Intent i = new Intent("com.evollu.react.fcm.ReceiveNotification");
-        i.putExtra("data", remoteMessage);
-        sendOrderedBroadcast(i, null);
+
+        Intent intent = new Intent("com.evollu.react.fcm.ReceiveNotification");
+
+        intent.putExtra("data", remoteMessage);
+
+        String imageUri = remoteMessage.getData().get("bigImage");
+
+        if (imageUri != null) {
+            Bitmap image = getBitmapfromUri(imageUri);
+            String message = remoteMessage.getData().get("message");
+
+            intent.putExtra("image", true);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+            Notification.Style style = new Notification.BigPictureStyle().bigPicture(image).setSummaryText(message);
+
+            Notification.Builder notificationBuilder = new Notification.Builder(this)
+                    .setSmallIcon(getSmallIcon())
+                    .setLargeIcon(getLargeIcon())
+                    .setStyle(style)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0, notificationBuilder.build());
+        } else {
+            sendOrderedBroadcast(intent, null);
+        }
+    }
+
+    public Bitmap getBitmapfromUri(String imageUri) {
+        try {
+            URL url = new URL(imageUri);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+
+            InputStream input = connection.getInputStream();
+
+            return BitmapFactory.decodeStream(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public int getSmallIcon() {
+        try {
+            ApplicationInfo applicationInfo = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+
+            return applicationInfo.metaData.getInt("com.google.firebase.messaging.default_notification_icon", 0);
+        } catch (NameNotFoundException e) {
+            return 0;
+        }
+    }
+
+    public Bitmap getLargeIcon() {
+        return ((BitmapDrawable) getPackageManager().getApplicationIcon(getApplicationInfo())).getBitmap();
     }
 }
